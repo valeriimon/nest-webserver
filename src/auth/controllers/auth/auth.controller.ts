45d5fb5ec78e79bc1a6d167112f5291d90b1974e
valeriimon <valeraaaaa94@gmail.com>
2018-12-24 14:11:39 +0200

    Registration process

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UserService } from 'users/user.service';
 import { AuthService } from 'auth/services/auth.service';
 import { getResponse, CustomError } from 'shared/utils/utils';
+import { ValidationPipe } from 'shared/pipes/validation/validation.pipe';
+import { User } from 'users/user.entity';
 
 
 @Controller('auth')
@@ -21,11 +23,11 @@ export class AuthController {
         const user = await this.userService
             .getUser(loginData)
             .then((user) => {
-                if(!user) throw 'User is not found';
+                if(!user) throw {message: 'User is not found'};
                 return user
             })
             .catch(err => {
-                error.err_stack.user_err = err
+                error.err_stack.user_err = err.message;
                 return null;
             });
         
@@ -38,5 +40,29 @@ export class AuthController {
             Object.keys(error.err_stack).length ? Promise.reject(error) : Promise.resolve({user, token}),
             'Login'
         )
+    }
+
+    @Post('register')
+    async register(@Body(new ValidationPipe(User)) body: User | Response) {
+        if(!(body instanceof User)) {
+            return body as Response
+        }
+        const error: CustomError = {
+            err_stack: {}
+        }
+        let token = '';
+        const user = await this.userService
+            .createUser(body)
+            .catch(err => error.err_stack.user_err = err.message);
+        if(user) {
+            token = await this.authService
+                .signIn({email: user.email})
+                .catch(err => error.err_stack.token_err = err.message);
+        }
+        
+
+        return await getResponse(
+            Object.keys(error.err_stack).length ? Promise.reject(error) : Promise.resolve({user, token}), 
+            'Registration');
     }
 }
